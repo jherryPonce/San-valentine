a3b289d6871d1d84c25567764a4dd9ae0335008e
jherrypg_pm <jherrypg@perumasivo.net>
2025-02-10 01:11:22 -0500

    PRUEBA DESPLIEGUE

--- script.js
+++ script.js
@@ -1,51 +1,37 @@
 const button = document.getElementById("movableButton");
-button.addEventListener("touchstart", moveButton); // Para móviles
-button.addEventListener("mouseenter", moveButton);
+button.addEventListener("touchstart", moveButton); 
+button.addEventListener("mouseenter", moveButton); 
 
-button.addEventListener("mouseenter", () => {
-  const maxX = window.innerWidth - button.offsetWidth;
-  const maxY = window.innerHeight - button.offsetHeight;
 
-  const newX = Math.random() * maxX;
-  const newY = Math.random() * maxY;
-
-  button.style.position = "absolute";
-  button.style.left = `${newX}px`;
-  button.style.top = `${newY}px`;
-});
-
-const path = window.location.pathname;
-const nombreSPLIT = path.split("/")[1] || "AMOR";
+const path = window.location.pathname; 
+const nombreSPLIT = path.split("/")[1] || "AMOR"; 
 const nombreFormateado = decodeURIComponent(nombreSPLIT).replace(/-/g, " ");
-const nombre =
-  nombreFormateado.charAt(0).toUpperCase() + nombreFormateado.slice(1);
+const nombre = nombreFormateado.charAt(0).toUpperCase() + nombreFormateado.slice(1);
+
 
 const preguntaElement = document.getElementById("pregunta");
 if (preguntaElement) {
   preguntaElement.textContent = `¿${nombre}, QUIERES SER MI SAN VALENTÍN?`;
 }
 
-window.addEventListener(
-  "click",
-  () => {
+
+window.addEventListener("click", () => {
     const audio = document.getElementById("miAudio");
     audio.muted = false;
-  },
-  { once: true }
-); // Solo ejecuta esto una vez
+}, { once: true }); // Solo ejecuta esto una vez
+
 
 const moveButton = () => {
-  const maxX = window.innerWidth - button.offsetWidth;
-  const maxY = window.innerHeight - button.offsetHeight;
+    const maxX = window.innerWidth - button.offsetWidth;
+    const maxY = window.innerHeight - button.offsetHeight;
 
-  const newX = Math.random() * maxX;
-  const newY = Math.random() * maxY;
+    const newX = Math.random() * maxX;
+    const newY = Math.random() * maxY;
 
-  button.style.position = "absolute";
-  button.style.left = `${newX}px`;
-  button.style.top = `${newY}px`;
-};
+    button.style.position = "absolute";
+    button.style.left = `${newX}px`;
+    button.style.top = `${newY}px`;
 
-button.addEventListener("click", () => {
-  alert("AL MENOS LO INTENTE");
-});
+  };
+
+  
