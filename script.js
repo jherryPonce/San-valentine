1cb8b739087cd129a480f233bb7d1c6dacf96858
jherrypg_pm <jherrypg@perumasivo.net>
2025-02-10 01:08:56 -0500

    PRUEBA DESPLIEGUE

--- script.js
+++ script.js
@@ -1,51 +1,51 @@
 const button = document.getElementById("movableButton");
+button.addEventListener("touchstart", moveButton); // Para móviles
+button.addEventListener("mouseenter", moveButton);
 
 button.addEventListener("mouseenter", () => {
-    const maxX = window.innerWidth - button.offsetWidth;
-    const maxY = window.innerHeight - button.offsetHeight;
+  const maxX = window.innerWidth - button.offsetWidth;
+  const maxY = window.innerHeight - button.offsetHeight;
 
-    const newX = Math.random() * maxX;
-    const newY = Math.random() * maxY;
+  const newX = Math.random() * maxX;
+  const newY = Math.random() * maxY;
 
-    button.style.position = "absolute";
-    button.style.left = `${newX}px`;
-    button.style.top = `${newY}px`;
-
-
-  
+  button.style.position = "absolute";
+  button.style.left = `${newX}px`;
+  button.style.top = `${newY}px`;
 });
 
-button.addEventListener("click",()=>{
-
-    alert('AL MENOS LO INTENTE');
-})
-
-
-// const path = window.location.pathname; 
-// const nombreSPLIT = path.split("/")[1] || "AMOR"; 
-// const nombreFormateado = decodeURIComponent(nombreSPLIT).replace(/-/g, " ");
-// const nombre = nombreFormateado.charAt(0).toUpperCase() + nombreFormateado.slice(1);
-
-// document.getElementById("pregunta").textContent = `¿${nombre}, QUIERES SER MI SAN VALENTÍN?`;
-
-
-const path = window.location.pathname; 
-
-// Asegurarse de que hay un nombre en la URL o usar un valor predeterminado
-const nombreSPLIT = path.split("/")[1] || "AMOR"; 
-
-// Decodificar el nombre, manejar guiones y formatear
+const path = window.location.pathname;
+const nombreSPLIT = path.split("/")[1] || "AMOR";
 const nombreFormateado = decodeURIComponent(nombreSPLIT).replace(/-/g, " ");
-const nombre = nombreFormateado.charAt(0).toUpperCase() + nombreFormateado.slice(1);
+const nombre =
+  nombreFormateado.charAt(0).toUpperCase() + nombreFormateado.slice(1);
 
-// Actualizar el texto en la página
 const preguntaElement = document.getElementById("pregunta");
 if (preguntaElement) {
   preguntaElement.textContent = `¿${nombre}, QUIERES SER MI SAN VALENTÍN?`;
 }
 
-
-window.addEventListener("click", () => {
+window.addEventListener(
+  "click",
+  () => {
     const audio = document.getElementById("miAudio");
     audio.muted = false;
-}, { once: true }); // Solo ejecuta esto una vez
+  },
+  { once: true }
+); // Solo ejecuta esto una vez
+
+const moveButton = () => {
+  const maxX = window.innerWidth - button.offsetWidth;
+  const maxY = window.innerHeight - button.offsetHeight;
+
+  const newX = Math.random() * maxX;
+  const newY = Math.random() * maxY;
+
+  button.style.position = "absolute";
+  button.style.left = `${newX}px`;
+  button.style.top = `${newY}px`;
+};
+
+button.addEventListener("click", () => {
+  alert("AL MENOS LO INTENTE");
+});
