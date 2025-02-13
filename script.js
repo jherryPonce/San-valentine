0f8b07b677b73284e49993edd4f02749a2a73a92
jherrypg_pm <jherrypg@perumasivo.net>
2025-02-13 05:01:14 -0500

    fix

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-const path = window.location.pathname; 
+const path = window.location.pathname;
 
-const nombreSPLIT = path.split("/")[1] || "AMOR"; 
+const nombreSPLIT = path.split("/")[1] || "AMOR";
 
 const nombreFormateado = decodeURIComponent(nombreSPLIT).replace(/-/g, " ");
-const nombre = nombreFormateado.charAt(0).toUpperCase() + nombreFormateado.slice(1).toUpperCase();
-
-
+const nombre =
+  nombreFormateado.charAt(0).toUpperCase() +
+  nombreFormateado.slice(1).toUpperCase();
 
 const preguntaElement = document.getElementById("pregunta");
 if (preguntaElement) {
   preguntaElement.textContent = `¿${nombre}, QUIERES SER MI SAN VALENTÍN?`;
 }
-
 
 //mensajes de rechazo
 const mensajes = [
@@ -31,49 +30,44 @@ let contador = 0;
 const button = document.getElementById("movableButton");
 
 button.addEventListener("mouseenter", () => {
-    const maxX = window.innerWidth - button.offsetWidth;
-    const maxY = window.innerHeight - button.offsetHeight;
+  const maxX = window.innerWidth - button.offsetWidth;
+  const maxY = window.innerHeight - button.offsetHeight;
 
-    const newX = Math.random() * maxX;
-    const newY = Math.random() * maxY;
+  const newX = Math.random() * maxX;
+  const newY = Math.random() * maxY;
 
-    button.style.position = "absolute";
-    button.style.left = `${newX}px`;
-    button.style.top = `${newY}px`;
+  button.style.position = "absolute";
+  button.style.left = `${newX}px`;
+  button.style.top = `${newY}px`;
 
-    // Habilita la reproducción en la primera interacción
-enableAudioOnInteraction();
-
+  // Habilita la reproducción en la primera interacción
+  enableAudioOnInteraction();
 });
-
 
 button.addEventListener("touchstart", () => {
-    const maxX = window.innerWidth - button.offsetWidth;
-    const maxY = window.innerHeight - button.offsetHeight;
+  const maxX = window.innerWidth - button.offsetWidth;
+  const maxY = window.innerHeight - button.offsetHeight;
 
-    const newX = Math.random() * maxX;
-    const newY = Math.random() * maxY;
+  const newX = Math.random() * maxX;
+  const newY = Math.random() * maxY;
 
-    button.style.position = "absolute";
-    button.style.left = `${newX}px`;
-    button.style.top = `${newY}px`;
-    // Habilita la reproducción en la primera interacción
-enableAudioOnInteraction();
-
+  button.style.position = "absolute";
+  button.style.left = `${newX}px`;
+  button.style.top = `${newY}px`;
+  // Habilita la reproducción en la primera interacción
+  enableAudioOnInteraction();
 });
 
-
-
 button.addEventListener("click", cambiarTexto);
-button.addEventListener("touchstart", cambiarTexto); 
+button.addEventListener("touchstart", cambiarTexto);
 
 function cambiarTexto() {
   if (contador < mensajes.length) {
-    button.innerText = mensajes[contador]; 
+    button.innerText = mensajes[contador];
     contador++;
   } else {
-    button.innerText = "Ok... 💔"; 
-    contador = 0
+    button.innerText = "No me rendire... 💔";
+    contador = 0;
   }
 }
 
@@ -92,60 +86,63 @@ const enableAudioOnInteraction = () => {
   document.body.addEventListener("touchstart", enableAudio, { once: true });
 };
 
+//PORTADA
 
-  //PORTADA
+document.getElementById("generarEnlace").addEventListener("click", function () {
+  const nombre = document.getElementById("nombreInput").value.trim();
+  const numero = document.getElementById("numeroInput").value.trim();
 
-  document.getElementById("generarEnlace").addEventListener("click", function () {
-    const nombre = document.getElementById("nombreInput").value.trim();
-    const numero = document.getElementById("numeroInput").value.trim();
-    
-    if (nombre === "" || numero === "") {
-      alert("Por favor, ingresa el nombre y el número de WhatsApp.");
-      return;
-    }
+  if (nombre === "" || numero === "") {
+    alert("Por favor, ingresa el nombre y el número de WhatsApp.");
+    return;
+  }
 
-   // Formatear nombre para URL
-   const nombreFormateado = encodeURIComponent(nombre.replace(/\s+/g, "-"));
+  // Formatear nombre para URL
+  const nombreFormateado = encodeURIComponent(nombre.replace(/\s+/g, "-"));
 
-   // Obtener la URL actual del sitio
-   const baseUrl = window.location.origin;
-   const enlaceFinal = `${baseUrl}/${nombreFormateado}`;
+  // Obtener la URL actual del sitio
+  const baseUrl = window.location.origin;
+  const enlaceFinal = `${baseUrl}/${nombreFormateado}`;
 
-   // Mostrar loader
-   document.getElementById("loader").style.display = "block";
-   
-   setTimeout(() => {
-     // Ocultar loader y mostrar el enlace generado
-     document.getElementById("loader").style.display = "none";
-     document.getElementById("enlaceCard").style.display = "block";
-     
-     const enlaceElemento = document.getElementById("enlaceGenerado");
-     enlaceElemento.href = enlaceFinal;
-     enlaceElemento.textContent = enlaceFinal;
-   }, 1500); // Simulación de carga
+  // Mostrar loader
+  document.getElementById("loader").style.display = "block";
 
-   // Enviar por WhatsApp
-   document.getElementById("enviarWhatsApp").addEventListener("click", function () {
-    const mensaje = `💖 ¡Hola, ${nombreFormateado} ! 💖\n\n✨ Quiero compartir contigo algo muy especial ✨\n\n🌹 Este enlace está lleno de amor y cariño, creado solo para ti: ${enlaceFinal} 💌\n\n😍 Espero que te guste tanto como me gustas tú. ❤️`;
-    const whatsappUrl = `https://wa.me/${numero}?text=${encodeURIComponent(mensaje)}`;
-    window.open(whatsappUrl, "_blank");
-  });
-  
- });
+  setTimeout(() => {
+    // Ocultar loader y mostrar el enlace generado
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("enlaceCard").style.display = "block";
 
+    const enlaceElemento = document.getElementById("enlaceGenerado");
+    enlaceElemento.href = enlaceFinal;
+    enlaceElemento.textContent = enlaceFinal;
+  }, 1500); // Simulación de carga
 
+  // Enviar por WhatsApp
+  document
+    .getElementById("enviarWhatsApp")
+    .addEventListener("click", function () {
+      const mensaje = `💖 ¡Hola, ${nombreFormateado} ! 💖\n\n✨ Quiero compartir contigo algo muy especial ✨\n\n🌹 Este enlace está lleno de amor y cariño, creado solo para ti: ${enlaceFinal} 💌\n\n😍 Espero que te guste tanto como me gustas tú. ❤️`;
+      const whatsappUrl = `https://wa.me/${numero}?text=${encodeURIComponent(
+        mensaje
+      )}`;
+      window.open(whatsappUrl, "_blank");
+    });
 
-  document.addEventListener("DOMContentLoaded", () => {
-    const path = window.location.pathname;
-    const nombreSPLIT = path.split("/")[1];
+  nombre.reset();
+  numero.reset();
+});
 
-    if (nombreSPLIT) {
-        document.getElementById("portada").classList.add("hidden");
-        document.getElementById("contenedor").classList.remove("hidden");
-        document.getElementById("contenedor").style.display = "flex";
-    }else{
-      setInterval(generarCorazones, 1000);
-    }
+document.addEventListener("DOMContentLoaded", () => {
+  const path = window.location.pathname;
+  const nombreSPLIT = path.split("/")[1];
+
+  if (nombreSPLIT) {
+    document.getElementById("portada").classList.add("hidden");
+    document.getElementById("contenedor").classList.remove("hidden");
+    document.getElementById("contenedor").style.display = "flex";
+  } else {
+    setInterval(generarCorazones, 1000);
+  }
 });
 
 function generarCorazones() {
@@ -167,12 +164,12 @@ function generarCorazones() {
 // boton acepto
 
 document.getElementById("fixedButton").addEventListener("click", function () {
-
   enableAudioOnInteraction();
 
   // Cambiar el mensaje principal
-  document.getElementById("pregunta").innerText = "¡Gracias por ser mi San Valentín! ❤️";
-  
+  document.getElementById("pregunta").innerText =
+    "¡Gracias por ser mi San Valentín! ❤️";
+
   // Mostrar mensaje de amor
   let mensaje = document.createElement("h2");
   mensaje.id = "mensajeAmor";
@@ -185,6 +182,4 @@ document.getElementById("fixedButton").addEventListener("click", function () {
 
   document.getElementById("movableButton").classList.add("hidden");
   document.getElementById("fixedButton").classList.add("hidden");
-
-
 });
