5d85606cd71a95b44f7b1587808aa7b539b79f39
jherrypg_pm <jherrypg@perumasivo.net>
2025-02-13 22:04:12 -0500

    fix

--- script.js
+++ script.js
@@ -129,6 +129,7 @@ document.getElementById("generarEnlace").addEventListener("click", function () {
   document
     .getElementById("enviarWhatsApp")
     .addEventListener("click", function () {
+      console.log(numero)
       const mensaje = `💖 ¡Hola, ${nombreFormateado} ! 💖\n\n✨ Quiero compartir contigo algo muy especial ✨\n\n🌹 Este enlace está lleno de amor y cariño, creado solo para ti: ${enlaceFinal} 💌\n\n😍 Espero que te guste tanto como me gustas tú. ❤️`;
       const whatsappUrl = `https://wa.me/${numero}?text=${encodeURIComponent(
         mensaje
